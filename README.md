8b33cf838edabdef501da98eeb68be8bfc7761c7
Gong Xiao <gxiao@diagnoa.com>
2019-07-02 00:42:34 +0800

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# vscode-cookiecutter
+# Cookiecutter for Visual Studio Code
+
+Visual Studio Code extension for running [cookiecutter](https://github.com/audreyr/cookiecutter) to creates projects from project templates.
+
+### Some tips:
+* tested on macOS Mojave.
+* for running cookiecutter, use explorer in vscode, select a folder, right click and select "Cookiecutter ..."
+
+### Installation Steps: 
+* First install [cookiecutter](https://github.com/audreyr/cookiecutter).
+* Add this configuration to your config file ($HOME/.cookiecutterrc):
+
+    ```
+    cookiecutters_dir: "XXX"
+    ```
+
+* install the extention.
+
+[Bug Reports](https://github.com/gongxiao/vscode-cookiecutter/issues)  
+[Repo](https://github.com/gongxiao/vscode-cookiecutter)
---
+++ extension.js
@@ -0,0 +1,58 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+'use strict';
+var vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    var Cookiecutter = vscode.commands.registerCommand('extension.cookiecutter', function (param) {
+        let term = vscode.Terminal;
+
+        const ccConf = path.join(process.env.HOME, ".cookiecutterrc");
+        var tmpDir = yaml.safeLoad(fs.readFileSync(ccConf, 'utf8')).cookiecutters_dir;
+        var re = /^~/;
+        const ccDir = tmpDir.replace(re, process.env.HOME);
+        const items = (srcPath) => {
+            return fs.readdirSync(srcPath).filter(file => fs.lstatSync(path.join(srcPath, file)).isDirectory())
+        }
+        const template = (ccDir) => {
+            return vscode.window.showQuickPick(items(ccDir), {
+                placeHolder: "Choosing a template.",
+                ignoreFocusOut: true,
+            });
+        };
+        template(ccDir).then(selection => {
+            // the user canceled the selection
+            if (!selection) {
+                return;
+            }
+
+            // the user selected some item. You could use `selection.name` too
+            var command = "".concat("cookiecutter -o ", param.path, " ", selection);
+            
+            if (vscode.window.terminals.length < 1) {
+                term = vscode.window.createTerminal("bash");
+            } else {
+                term = vscode.window.activeTerminal;
+            }
+            message = command;
+            term.show();
+            term.sendText(command);
+        });
+
+    });
+    context.subscriptions.push(Cookiecutter);
+
+}
+
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
+
+
